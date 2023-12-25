116c8ac1420ffcc66047e3a2a6cd04333765b211
Yuva Mikkilineni <yuvam@domain.hill-labs.com>
2023-12-26 01:27:43 +1300

    This is the initial commit for the TurnUp Portal Automation

---
+++ TurnUpPortalAutomationYuva/Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+//Open Chrome Browser
+IWebDriver driver = new ChromeDriver();
+
+//Maximize the browser
+driver.Manage().Window.Maximize();
+
+//Launch TurnUp Portal and navigate to login page (http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f)
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+//identify username textbox and enter valid username
+IWebElement usernnameTextBox = driver.FindElement(By.Id("UserName"));
+usernnameTextBox.SendKeys("hari");
+
+//identify password textbox and enter valid username
+IWebElement passwordTextBox = driver.FindElement(By.Id("Password"));
+passwordTextBox.SendKeys("123123");
+
+//identify the login button and click on the button
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+
+//Check if user has logged in successfully
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("User has logged in successfully");
+}
+else
+{
+    Console.WriteLine("User hasn't ben logged in...");
+}
