f9f6437150271369e2632fcb8ea4ba9b67125969
Yuva Mikkilineni <yuvam@domain.hill-labs.com>
2024-01-10 20:30:13 +1300

    Updated with Time And Material Create/Edit/Delete

--- TurnUpPortalAutomationYuva/Program.cs
+++ TurnUpPortalAutomationYuva/Program.cs
@@ -35,3 +35,104 @@ else
 {
     Console.WriteLine("User hasn't ben logged in...");
 }
+
+
+
+
+//Navigate to Time and Material Module
+IWebElement adminstrationDropDown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+adminstrationDropDown.Click();
+
+IWebElement TMoption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+TMoption.Click();
+
+//Click on Create new button
+IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createNewButton.Click();
+
+//Select Time from TypeCode
+IWebElement typeDropDown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span"));
+typeDropDown.Click();
+
+IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+timeOption.Click();
+
+//Input Code
+IWebElement codeTextBox = driver.FindElement(By.Id("Code"));
+codeTextBox.SendKeys("January2024-Y");
+
+//Input Description
+IWebElement descriptionTextBox = driver.FindElement(By.Id("Description"));
+descriptionTextBox.SendKeys("January2024-Y");
+
+//Identify the Price TextBox and enter
+IWebElement priceTextBox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+priceTextBox.SendKeys("100");
+
+//Click on Save Button
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+Thread.Sleep(2000);
+
+//Click on Last Page button
+IWebElement goToLastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPage.Click();
+Thread.Sleep(2000);
+
+//Check if record is present
+IWebElement actualCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+
+if  (actualCode.Text == "January2024-Y")
+{
+    Console.WriteLine("The Record Created Successfully");
+}else
+{
+    Console.WriteLine("Record Not Created");
+}
+
+//Edit the Record January2024-Y
+IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+editButton.Click();
+
+descriptionTextBox = driver.FindElement(By.Id("Description"));
+descriptionTextBox.Clear();
+descriptionTextBox.SendKeys("Automation Testing- Yuva");
+
+saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+Thread.Sleep(2000);
+
+goToLastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPage.Click();
+Thread.Sleep(2000);
+
+actualCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[3]"));
+
+if (actualCode.Text == "Automation Testing- Yuva")
+{
+    Console.WriteLine("Updated successfull");
+}else
+{
+    Console.WriteLine("Record Not Updated");
+}
+
+//Delete the Record January2024-Y
+IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
+deleteButton.Click();
+
+IAlert deleteAlert = driver.SwitchTo().Alert();
+
+//string text = deleteAlert.Text;
+deleteAlert.Accept();
+
+actualCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()-1]/td[1]"));
+
+if (actualCode.Text != "January2024-Y")
+{
+    Console.WriteLine("Deleted successfull");
+}
+else
+{
+    Console.WriteLine("Record Not Deleted");
+}
